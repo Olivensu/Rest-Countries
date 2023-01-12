c5c7c0be83af0d1ac0e1ee7a184534e3673287fb
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-01-12 20:53:26 +0600

    Work properly

--- src/components/COuntries/Countries.js
+++ src/components/COuntries/Countries.js
@@ -1,19 +1,22 @@
 import React, {useState, useEffect } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
-    const [Countries, setCountries] = useState();
+    const [Countries, setCountries] = useState([]);
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all').then(res => res.json())
     .then(data =>setCountries(data));
     }, [])
     console.log(Countries);
     return (
-        <div>
-            <h1>Hi I am the best</h1>
+        <div >
+            <h1>Here is the All country list...</h1>
+            <div className='countries'>
             {
-                Countries.map(country => console.log(country))
+                Countries.map((country, index) => <Country country={country} index={index}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Country.css'
 const Country = (props) => {
     console.log(props)
-    // const {area, population, flag } =props.country
+    const {area, population, flags, name } =props.country
     return (
         <div className='country'>
-            <h1>Sam is best</h1>
-            {/* <h3>Country Name: {area}</h3>
+            <h5>Country Name: {name.common}</h5>
             <p>Population: {population}</p>
-            <img src={flag} alt="" /> */}
+            <p>Area: {area}</p>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
